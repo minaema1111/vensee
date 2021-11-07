3d608e4fb938705fdb62dbf51afefa9ec668135f
みなえま <mina.ema.lol@gmail.com>
2021-11-07 12:30:20 +0900

    コメントアウト削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,12 @@ class ApplicationController < ActionController::Base
   end
 
   def search
-    # @venders = Vender.search(params[:search])
-    # method = params[:search_method]
-    # word = params[:search_word]
-    # @p = Vender.search(method,word)
-    # @p = Vender.search(:residence_cont => params[:q][:search])
+
     @results = @p.result
   end
 
   private
   def create_searching_object
-    # genre_idが1の場合は検索条件から外す
     if params[:q].present? && params[:q][:genre_id_eq] == "1"
       params[:q][:genre_id_eq] = ''
     end
