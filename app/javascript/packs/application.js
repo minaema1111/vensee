59b37eb9d0f1293451489dfd2288250f468e9713
minaema1111 <mina.ema.lol@gmail.com>
2021-11-12 17:04:56 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,4 +11,4 @@ import "../css/tailwindcss.css";
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "stylesheets/application"
+// import "stylesheets/application"
