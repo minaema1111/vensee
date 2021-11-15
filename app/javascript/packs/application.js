51a7e5ed55e7a93ee3cae2df3066fbc06d639cdf
minaema1111 <mina.ema.lol@gmail.com>
2021-11-15 14:51:25 +0900

    tailwind修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "../css/tailwindcss.css";
+import "../css/tailwind.css";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
